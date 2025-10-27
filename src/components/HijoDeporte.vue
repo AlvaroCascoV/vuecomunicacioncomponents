d4eb733eabd81aa8cb5f5c5aec582dad290bccce
AlvaroCascoV <alvarocascovalero@gmail.com>
2025-10-27 14:14:31 +0100

    comunicacion padre-hijo

---
+++ src/components/HijoDeporte.vue
@@ -0,0 +1,12 @@
+<template>
+	<h4 style="color: red">{{ nombredeporte }}</h4>
+</template>
+<script>
+	export default {
+		name: "HijoDeporte",
+		props: ["nombredeporte"],
+		mounted() {
+			console.log(this.nombredeporte);
+		},
+	};
+</script>
---
+++ src/components/PadreDeportes.vue
@@ -0,0 +1,23 @@
+<template>
+	<h1 style="color: blue">Padre deportes</h1>
+	<ul>
+		<li v-for="sport in deportes" :key="sport">
+			<HijoDeporte :nombredeporte="sport" />
+		</li>
+	</ul>
+</template>
+<script>
+	import HijoDeporte from "./HijoDeporte.vue";
+
+	export default {
+		name: "PadreDeportes",
+		components: {
+			HijoDeporte,
+		},
+		data() {
+			return {
+				deportes: ["Curling", "Peonza", "Canicas", "Bolos"],
+			};
+		},
+	};
+</script>
