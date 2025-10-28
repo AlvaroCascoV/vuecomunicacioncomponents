2a9debd9006abafb1c63494ca5dce5b18ec0d6b3
AlvaroCascoV <alvarocascovalero@gmail.com>
2025-10-28 09:36:36 +0100

    comunicacion metodos

--- src/components/HijoDeporte.vue
+++ src/components/HijoDeporte.vue
@@ -1,5 +1,6 @@
 <template>
 	<h4 style="color: red">{{ nombredeporte }}</h4>
+	<button v-on:click="seleccionarFavorito">Seleccionar Favorito</button>
 </template>
 <script>
 	export default {
@@ -7,6 +8,14 @@
 		props: ["nombredeporte"],
 		mounted() {
 			console.log(this.nombredeporte);
+		},
+		methods: {
+			seleccionarFavorito() {
+				console.log("Soy el hijo " + this.nombredeporte);
+				//PARA ENVIAR INFORMACION SE REALIZA A PARTIR DE LA LLAMADA
+				//CON CADA PARAMETRO SEPARADO CON COMAS, DENTRO DEL METODO $emit
+				this.$emit("seleccionarFavoritoParent", this.nombredeporte);
+			},
 		},
 	};
 </script>
--- src/components/PadreDeportes.vue
+++ src/components/PadreDeportes.vue
@@ -1,11 +1,20 @@
 <template>
-	<h1 style="color: blue">Padre deportes</h1>
-	<ul>
-		<li v-for="sport in deportes" :key="sport">
-			<HijoDeporte :nombredeporte="sport" />
-		</li>
-	</ul>
+	<div>
+		<h1 style="color: blue">Padre deportes</h1>
+		<h2 style="background-color: yellow" v-if="favorito">
+			Su deporte favorito es {{ favorito }}
+		</h2>
+		<ul>
+			<li v-for="sport in deportes" :key="sport">
+				<HijoDeporte
+					:nombredeporte="sport"
+					v-on:seleccionarFavoritoParent="seleccionarFavoritoParent"
+				/>
+			</li>
+		</ul>
+	</div>
 </template>
+
 <script>
 	import HijoDeporte from "./HijoDeporte.vue";
 
@@ -17,7 +26,14 @@
 		data() {
 			return {
 				deportes: ["Curling", "Peonza", "Canicas", "Bolos"],
+				favorito: null,
 			};
+		},
+		methods: {
+			seleccionarFavoritoParent(deporteFavorito) {
+				console.log("Yo soy tu padre");
+				this.favorito = deporteFavorito;
+			},
 		},
 	};
 </script>
